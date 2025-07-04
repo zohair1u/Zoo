ae81e3571aadd6d488f944de8da8f516789310ff
zohair1u <zouhir.piniz@gmail.com>
2025-07-04 15:56:05 +0100

    Create Users

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,21 +2,74 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserTypeForm;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 final class MainController extends AbstractController
 {
+    #[Route('/', name: 'home')]
+    public function indexHome(): Response
+    {
+        return $this->render('\main\index.html.twig');
+    }
+
+    #[Route('/admin/users', name: 'users')]
+    public function createUser(HttpFoundationRequest $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em): Response
+    {
+    $user = new User();
+    $form = $this->createForm(UserTypeForm::class, $user);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $hashedPassword = $passwordHasher->hashPassword($user, $user->getPassword());
+        $user->setPassword($hashedPassword);
+        // Pour transformer le string en tableau mais j'ai réglé le souci sur le UserForm directement :
+        // $user->setRoles([$form->get('roles')->getData()]);
+        $user->setCreatedAt(new \DateTime());
+        $user->setUpdatedAt(null);
+        $em->persist($user);
+        $em->flush();
+
+        $this->addFlash('success', 'Utilisateur créé avec succès.');
+        return $this->redirectToRoute('users');
+    }
+
+    return $this->render('main/admin/createUser.html.twig', [
+        'form' => $form->createView(),
+    ]);
+    }
+
     #[Route('/admin', name: 'dashboard')]
-    public function index(): Response
+    public function indexAdmin(): Response
     {
         return $this->render('main/admin/dashboard.html.twig');
     }
 
-    #[Route('/lol', name: 'app_main')]
-    public function index2(): Response
+//Redirection en fonction de type de l'utilisateur :
+    #[Route('/apres-connexion', name: 'apres_connexion')]
+    public function redirectionPostLogin(): Response
     {
-        return $this->render('main/index.html.twig');
+        $user = $this->getUser();
+
+        if (in_array('ROLE_ADMIN', $user->getRoles(), true)) {
+            return $this->redirectToRoute('dashboard');
+    }
+
+        if (in_array('ROLE_EMPLOYE', $user->getRoles(), true)) {
+            return $this->redirectToRoute('employe_dashboard');
+    }
+
+        if (in_array('ROLE_VETERINAIRE', $user->getRoles(), true)) {
+            return $this->redirectToRoute('veterinaire_dashboard');
+    }
+
+        return $this->redirectToRoute('homepage');
     }
 }
---
+++ src/Form/UserTypeForm.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserTypeForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('roles', ChoiceType::class, [
+            'choices' => [
+            'Administrateur' => 'ROLE_ADMIN',
+            'Employé' => 'ROLE_EMPLOYE',
+            'Vétérinaire' => 'ROLE_VETERINAIRE',
+             ],
+            //  'expanded' => false, 
+             'multiple' => true, 
+             'label' => 'Rôle de l’utilisateur',
+             ])
+
+            ->add('password', PasswordType::class)
+            ->add('nom')
+            ->add('prenom')
+            ->add('Ajouter', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
