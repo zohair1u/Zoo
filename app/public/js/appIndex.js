6adec6c46a28174a94aca533733633a442b78240
zohair1u <zouhir.piniz@gmail.com>
2025-07-11 16:09:02 +0100

    DarkMood

--- app/public/js/appIndex.js
+++ app/public/js/appIndex.js
@@ -53,3 +53,25 @@ document.addEventListener('DOMContentLoaded', () => {
         isSwiping = false;
 });
 });
+
+
+// DARK MODE : 
+
+  const toggleButton = document.getElementById('toggle-darkmode');
+  const icon = document.getElementById('icon-darkmode');
+
+  // Appliquer le mode selon localStorage
+  if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark-mode');
+    icon.src = '/images/sun-icon.png';
+    icon.alt = 'Activer le mode clair';
+}
+
+  toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    const isDark = document.body.classList.contains('dark-mode');
+    localStorage.setItem('theme', isDark? 'dark': 'light');
+
+    icon.src = isDark? '/images/sun-icon.png': '/images/moon-icon.png';
+    icon.alt = isDark? 'Activer le mode clair': 'Activer le mode sombre';
+});
